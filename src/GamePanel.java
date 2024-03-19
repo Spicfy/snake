97ed8f5be2a979b44a59de206e45dccadf1ce3d5
Spicfy <68024083+Spicfy@users.noreply.github.com>
2024-03-19 15:54:25 -0400

    snake

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,15 +1,46 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+
+
+
+
 public class GamePanel  extends JPanel implements ActionListener{
     GamePanel(){
 
     }
-    public void startGame(Graphics g){
+    public void startGame(){
 
     }
-    public void paintComponent(){
+    public void paintComponent(Graphics g){
 
     }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void actionPerformed(ActionEvent e){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+            
+        }
+    }
+    
 
 }
