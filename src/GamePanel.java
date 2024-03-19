b27a28968da9b40967858cc206847743e94effd4
Spicfy <68024083+Spicfy@users.noreply.github.com>
2024-03-19 03:51:43 -0400

    new snake project

---
+++ src/GamePanel.java
@@ -0,0 +1,15 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+public class GamePanel  extends JPanel implements ActionListener{
+    GamePanel(){
+
+    }
+    public void startGame(Graphics g){
+
+    }
+    public void paintComponent(){
+
+    }
+
+}
